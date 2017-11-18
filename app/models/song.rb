6d1ccc61c3728588a06c6c57dbd6ea8e06ff311b
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-18 17:49:00 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,6 +10,6 @@ class Song < ActiveRecord::Base
     # when this method is called it should assign the song's artist to Drake
     #binding.pry
     drake = Artist.new(name: "Drake")
-    #self.artist  = "Drake"
+    self.artist  = drake
   end
 end
